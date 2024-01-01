902f2dadc4d82730fb642b3764e0a129045aa8ce
Fozzyack <fsundra@gmail.com>
2024-01-01 22:15:49 +0800

    adding a cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,4 @@
+# For cleaning up runpod workspace (if  working with a low storage runpod)
+
+rm -rf /workspace/ComfyUI/output/*
+rm -rf /workspace/ComfyUI/temp/*
--- copyintovm.sh
+++ copyintovm.sh
@@ -4,4 +4,5 @@ ssh_id='YOUR SSH ID e.g id_ed123456'
 scp -P $port -i ~/.ssh/$ssh_id autodownload.sh root@$ip:/workspace
 scp -P $port -i ~/.ssh/$ssh_id customNodeGit.sh root@$ip:/workspace
 scp -P $port -i ~/.ssh/$ssh_id optionalDelete.sh root@$ip:/workspace
+scp -P $port -i ~/.ssh/$ssh_id cleanup.sh root@$ip:/workspace
 ssh root@$ip -p $port -i ~/.ssh/$ssh_id
