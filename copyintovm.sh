8c80268f88bd1ec9521987bee518d76a0e928ec8
Fozzyack <fsundra@gmail.com>
2024-01-01 22:08:28 +0800

    yeew

--- copyintovm.sh
+++ copyintovm.sh
@@ -1,6 +1,7 @@
 port='YOUR RUNPOD PORT'
 ip='YOUR RUNPOD IP'
-scp -P $port -i ~/.ssh/------- autodownload.sh root@$ip:/workspace
-scp -P $port -i ~/.ssh/------- customNodeGit.sh root@$ip:/workspace
-scp -P $port -i ~/.ssh/------- optionalDelete.sh root@$ip:/workspace
-ssh root@38.147.83.14 -p 35008 -i ~/.ssh/-------
+ssh_id='YOUR SSH ID e.g id_ed123456'
+scp -P $port -i ~/.ssh/$ssh_id autodownload.sh root@$ip:/workspace
+scp -P $port -i ~/.ssh/$ssh_id customNodeGit.sh root@$ip:/workspace
+scp -P $port -i ~/.ssh/$ssh_id optionalDelete.sh root@$ip:/workspace
+ssh root@$ip -p $port -i ~/.ssh/$ssh_id
