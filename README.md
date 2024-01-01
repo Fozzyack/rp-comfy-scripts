47b63bd5a9ad1080f9d4a515deac3917e798652e
Fozzyack <fsundra@gmail.com>
2024-01-01 22:00:39 +0800

    changes

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Some ComfyUI RunPod Scripts
-- Allows for the automated download of some files upon runload start
+- Allows for the automated download of some files upon runpod start
 - The IP and Port constantly changes for deployment so don't bother trying them
+- Downloads all models, upscalers, loras from [CivitAI](https://civitai.com/)
+- Custom Nodes are from github
+- Will be adding Control NET
+- Most models are SDXL (might move to only SDXL files)
+## Operation:
+1. copyintovm.sh
+2. Run Copied bash scripts in VM
--- copyintovm.sh
+++ copyintovm.sh
@@ -1,6 +1,6 @@
-port='35008'
-ip='38.147.83.14'
-scp -P $port -i ~/.ssh/id_ed25519 autodownload.sh root@$ip:/workspace
-scp -P $port -i ~/.ssh/id_ed25519 customNodeGit.sh root@$ip:/workspace
-scp -P $port -i ~/.ssh/id_ed25519 optionalDelete.sh root@$ip:/workspace
-ssh root@38.147.83.14 -p 35008 -i ~/.ssh/id_ed25519
+port='YOUR RUNPOD PORT'
+ip='YOUR RUNPOD IP'
+scp -P $port -i ~/.ssh/----- autodownload.sh root@$ip:/workspace
+scp -P $port -i ~/.ssh/----- customNodeGit.sh root@$ip:/workspace
+scp -P $port -i ~/.ssh/----- optionalDelete.sh root@$ip:/workspace
+ssh root@38.147.83.14 -p 35008 -i ~/.ssh/-----
